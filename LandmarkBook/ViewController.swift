0abe73cb05f61a2c761fc5db82e453e4366133bf
Taylor Gagne <taylor@baremetalcloud.com>
2020-08-31 17:20:01 -0400

    First commit

---
+++ LandmarkBook/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  LandmarkBook
+//
+//  Created by Taylor Gagne on 8/31/20.
+//  Copyright © 2020 Taylor Gagne. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "Test"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,1 @@
+# Landmark-IOS-App
